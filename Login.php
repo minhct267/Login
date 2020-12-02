8ebacbff059dc2a313c9f97c3eb31531d8900aa5
unknown <dnminhd18145@cusc.ctu.edu.vn>
2020-12-02 21:06:01 +0700

    Try1

--- Login.php
+++ Login.php
@@ -18,17 +18,5 @@
             <a href="#">Lost your password?</a><br>
             <a href="#">Don't have an account?</a>
         </form>
-<?php
-if(isset($_POST['btnLogin'])){
-    $username = $_POST['username']
-    $password = $_POST['password']
-    if($username=='minhct267' and $password=='Minhct267')
-    {
-        echo 'Dang nhap thanh cong'
-    } else echo 'Dang nhap that bai'
-}
-?>
-</div>
-
 </body>
 </html>
